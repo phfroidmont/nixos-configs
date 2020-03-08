0c54d0d7e7900bc8fd09ccd1250e8fd2279d5d70
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2020-03-08 20:55:29 +0100

    Add patch to fix Lutris with recent gtk3 version

--- configs/games.nix
+++ configs/games.nix
@@ -2,6 +2,16 @@
 {
   hardware.opengl.driSupport32Bit = true;
   home-manager.users.froidmpa = {pkgs, config, ...}: {
+    nixpkgs.config = {
+      allowUnfree = true;
+      packageOverrides = super: let self = super.pkgs; in {
+        lutris-unwrapped = super.lutris-unwrapped.overridePythonAttrs (oldAttrs: rec {
+          patches = [
+            ./lutris_sort_new_with_model_fix.patch
+          ];
+        });
+      };
+    };
     home.packages = with pkgs; [
       steam
       lutris
