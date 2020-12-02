b06047834ebd4267dd80370071ae47cbe6c0146f
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2020-12-02 15:25:37 +0100

    Remove duplicated or unecessary email config

--- configs/home/email.nix
+++ configs/home/email.nix
@@ -10,20 +10,6 @@ let
     davmail.smtpPort=1025
     davmail.disableUpdateCheck=true
   '';
-  davMailIngenicoConf = pkgs.writeText "davmail-ingenico.propertries" ''
-      davmail.server=true
-      davmail.mode=O365Modern
-      davmail.oauth.clientId=8ed87c30-ae92-4443-8528-f6a52ac95362
-      davmail.oauth.redirectUri=https://login.microsoftonline.com/common/oauth2/nativeclient
-      davmail.oauth.tenantId=143a5e19-140b-4632-b3ea-e71a15cddb8c
-      davmail.url=https://outlook.office365.com/EWS/Exchange.asmx
-      davmail.caldavPort=2080
-      davmail.imapPort=2143
-      davmail.ldapPort=2389
-      davmail.popPort=2110
-      davmail.smtpPort=2025
-      davmail.disableUpdateCheck=true
-    '';
 in {
   home.packages = with pkgs; [
     davmail
@@ -38,19 +24,6 @@ in {
         Type = "simple";
         RemainAfterExit = "no";
         ExecStart = "${pkgs.davmail}/bin/davmail ${davMailArhsConf}";
-      };
-    };
-  };
-  systemd.user.services = {
-    davmail-ingenico = {
-      Unit = {
-        Description = "DavMail Ingenico Exchange";
-      };
-
-      Service = {
-        Type = "simple";
-        RemainAfterExit = "no";
-        ExecStart = "${pkgs.davmail}/bin/davmail ${davMailIngenicoConf}";
       };
     };
   };
--- configs/network.nix
+++ configs/network.nix
@@ -3,12 +3,4 @@
   networking.hosts = {
     "127.0.0.1" = [ "localhost" "local.ted.europa.eu" ];
   };
-  services.davmail = {
-    enable = true;
-    url = "https://webmail.arhs-developments.com/owa/";
-    config = {
-      davmail.mode = "EWS";
-      davmail.allowRemote = false;
-    };
-  };
 }
