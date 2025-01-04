db78e0407ff003d3249dfa3aa86ba29e8dc8def5
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2025-01-04 01:16:38 +0100

    Add gamemode and mangohud

--- modules/media/games.nix
+++ modules/media/games.nix
@@ -14,12 +14,25 @@ in
     lutris.enable = lib.my.mkBoolOpt false;
   };
 
-  config = {
-    user.packages = [
-      (lib.mkIf cfg.steam.enable pkgs.steam)
-      (lib.mkIf cfg.lutris.enable pkgs.lutris)
-      (lib.mkIf cfg.lutris.enable pkgs.wine)
-      (lib.mkIf (cfg.steam.enable || cfg.lutris.enable) pkgs.protontricks)
-    ];
-  };
+  config = lib.mkMerge [
+    {
+      user.packages = [
+        (lib.mkIf cfg.steam.enable pkgs.steam)
+        (lib.mkIf cfg.lutris.enable pkgs.lutris)
+        (lib.mkIf cfg.lutris.enable pkgs.wine)
+        (lib.mkIf (cfg.steam.enable || cfg.lutris.enable) pkgs.protontricks)
+      ];
+    }
+    (lib.mkIf (cfg.steam.enable || cfg.lutris.enable) {
+      programs.gamemode = {
+        enable = true;
+        enableRenice = true;
+      };
+      home-manager.users.${config.user.name} = {
+        programs.mangohud = {
+          enable = true;
+        };
+      };
+    })
+  ];
 }
