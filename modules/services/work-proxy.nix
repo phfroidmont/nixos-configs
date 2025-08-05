bff91b2a42a94cb0e1ef9887413b9e102369cfd5
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2025-08-05 14:58:34 +0200

    Update work-proxy

--- modules/services/work-proxy.nix
+++ modules/services/work-proxy.nix
@@ -18,9 +18,9 @@ in
       enable = true;
       settings = {
         server = [
-          "/lefoyer.lu/10.33.0.100"
-          "/foyer.lu/10.33.0.100"
-          "/foyer.cloud/10.33.0.100"
+          "/lefoyer.lu/127.0.0.1#1053"
+          "/foyer.lu/127.0.0.1#1053"
+          "/foyer.cloud/127.0.0.1#1053"
           "1.1.1.1"
         ];
         no-resolv = true;
@@ -39,14 +39,52 @@ in
     services.tinyproxy = {
       enable = true;
       settings = {
+        LogLevel = "Info";
         Port = 2345;
         Upstream = [
-          ''upstream socks5 localhost:5080 ".lefoyer.lu"''
-          ''upstream socks5 localhost:5080 ".foyer.lu"''
-          ''upstream socks5 localhost:5080 ".foyer.cloud"''
-          ''upstream http localhost:3128 ".microsoftonline.com"''
+          ''upstream socks5 127.0.0.1:5080 ".lefoyer.lu"''
+          ''upstream socks5 127.0.0.1:5080 ".foyer.lu"''
+          ''upstream socks5 127.0.0.1:5080 ".foyer.cloud"''
+          ''upstream http   127.0.0.1:3128 ".microsoftonline.com"''
         ];
       };
+    };
+
+    services.redsocks = {
+      enable = false;
+      log_debug = true;
+      log_info = true;
+      redsocks = [
+        {
+          port = 12345;
+          proxy = "127.0.0.1:5080";
+          type = "socks5";
+          redirectCondition = "-d 10.134.0.0/16";
+          doNotRedirect = [
+            "-p tcp -m owner --uid-owner redsocks"
+            "-p tcp --dport 80"
+            "-p tcp --dport 443"
+          ];
+        }
+        # {
+        #   port = 12345;
+        #   proxy = "127.0.0.1:${toString config.services.tinyproxy.settings.Port}";
+        #   type = "http-relay";
+        #   redirectCondition = "--dport 80";
+        #   doNotRedirect = [
+        #     "-p tcp -m owner --uid-owner tinyproxy"
+        #   ];
+        # }
+        # {
+        #   port = 12346;
+        #   proxy = "127.0.0.1:${toString config.services.tinyproxy.settings.Port}";
+        #   type = "http-connect";
+        #   redirectCondition = "--dport 443";
+        #   doNotRedirect = [
+        #     "-p tcp -m owner --uid-owner tinyproxy"
+        #   ];
+        # }
+      ];
     };
 
     security.pki.certificateFiles = [
@@ -61,14 +99,16 @@ in
     };
 
     home-manager.users.${config.user.name} = {
-      home.file.".sbt/repositories".text = ''
-        [repositories]
-          local
-          maven-local
-          nexus-maven: https://nexus.foyer.lu/repository/mvn-all/
-          nexus-ivy: https://nexus.foyer.lu/repository/ivy-all/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]
-          nexus-ivy-sbt: https://nexus.foyer.lu/repository/ivy-all/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[artifact](-[classifier])-[type].[ext]
-      '';
+      home = {
+        file.".sbt/repositories".text = ''
+          [repositories]
+            local
+            maven-local
+            nexus-maven: https://nexus.foyer.lu/repository/mvn-all/
+            nexus-ivy: https://nexus.foyer.lu/repository/ivy-all/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[type]s/[artifact](-[classifier]).[ext]
+            nexus-ivy-sbt: https://nexus.foyer.lu/repository/ivy-all/, [organization]/[module]/(scala_[scalaVersion]/)(sbt_[sbtVersion]/)[revision]/[artifact](-[classifier])-[type].[ext]
+        '';
+      };
     };
 
     environment.systemPackages = with pkgs; [ chisel ];
