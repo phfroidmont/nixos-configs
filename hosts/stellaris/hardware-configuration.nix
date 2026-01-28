74d81c7d2b317e3bafde4933b7b191d38af25324
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-01-28 13:42:33 +0100

    Update inputs

--- hosts/stellaris/hardware-configuration.nix
+++ hosts/stellaris/hardware-configuration.nix
@@ -11,7 +11,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
