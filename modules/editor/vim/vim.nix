ec09c048ae76b51df95238bce137daca8c5c724e
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-03-26 00:02:46 +0100

    nvim: disable leap to get flash features back

--- modules/editor/vim/vim.nix
+++ modules/editor/vim/vim.nix
@@ -213,7 +213,6 @@ in
                   { import = "lazyvim.plugins.extras.ui.edgy" },
                   { import = "lazyvim.plugins.extras.coding.mini-comment" },
                   { import = "lazyvim.plugins.extras.editor.aerial" },
-                  { import = "lazyvim.plugins.extras.editor.leap" },
                   { import = "lazyvim.plugins.extras.editor.navic" },
                   { import = "lazyvim.plugins.extras.formatting.prettier" },
                   { import = "lazyvim.plugins.extras.lang.angular" },
