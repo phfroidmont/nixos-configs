c5abace3d9980331d03f8d4895e073b0af161cf5
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-03-25 01:03:37 +0100

    fix(nvim): use opencode server fallback in kitty and move mappings to <leader>o

--- modules/editor/vim/lua/plugins/opencode.lua
+++ modules/editor/vim/lua/plugins/opencode.lua
@@ -1,13 +1,26 @@
 return {
   "NickvanDyke/opencode.nvim",
   config = function()
+    local function open_opencode_in_kitty_tab()
+      vim.fn.jobstart({
+        "kitty",
+        "@",
+        "launch",
+        "--type=tab",
+        "--cwd",
+        vim.fn.getcwd(),
+        "sh",
+        "-lc",
+        "opencode --port",
+      }, { detach = true })
+    end
+
     ---@type opencode.Opts
     vim.g.opencode_opts = {
-      provider = {
-        enabled = "kitty",
-        kitty = {
-          location = "tab"
-        }
+      server = {
+        start = open_opencode_in_kitty_tab,
+        toggle = open_opencode_in_kitty_tab,
+        stop = function() end,
       },
       events = {
         permissions = {
@@ -17,19 +30,19 @@ return {
     }
     vim.o.autoread = true
 
-    vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+    vim.keymap.set({ "n", "x" }, "<leader>oa", function()
       require("opencode").ask("@this: ", { submit = true })
     end, { desc = "Ask opencode" })
 
-    vim.keymap.set({ "n", "x" }, "<leader>ao", function()
+    vim.keymap.set({ "n", "x" }, "<leader>oo", function()
       require("opencode").select()
     end, { desc = "Execute opencode action…" })
 
-    vim.keymap.set({ "n", "x" }, "<leader>as", function()
+    vim.keymap.set({ "n", "x" }, "<leader>os", function()
       require("opencode").prompt("@this")
     end, { desc = "Add to opencode" })
 
-    vim.keymap.set({ "n", "t" }, "<leader>a.", function()
+    vim.keymap.set({ "n", "t" }, "<leader>o.", function()
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
   end,
