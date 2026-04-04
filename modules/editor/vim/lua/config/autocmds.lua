9cfa2f84085198cf065698c7ddf8ab0fd6cdbbf8
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-04-05 00:35:01 +0200

    fix(nvim): autostart Tree-sitter highlighting for ledger journal files

--- modules/editor/vim/lua/config/autocmds.lua
+++ modules/editor/vim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local ts_ledger = vim.api.nvim_create_augroup("treesitter_ledger", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = ts_ledger,
+  pattern = { "ledger" },
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf, "ledger")
+  end,
+})
