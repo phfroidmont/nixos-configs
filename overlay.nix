4760290ecc6c4acb5cb75d34d06a3526a05fbe72
Paul-Henri Froidmont <git.contact-57n2p@froidmont.org>
2026-08-31 11:19:39 +0200

    fix(metals): pass javac module options to JVM

--- overlay.nix
+++ overlay.nix
@@ -1,4 +1,25 @@
 final: prev: {
   get-token = prev.callPackage ./packages/get-token/package.nix { };
+  metals = prev.metals.overrideAttrs (oldAttrs: {
+    extraJavaOpts = prev.lib.concatStringsSep " " [
+      oldAttrs.extraJavaOpts
+      "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.resources=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED"
+      "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
+      "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED"
+      "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED"
+      "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED"
+      "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED"
+    ];
+  });
   mia = prev.callPackage ./packages/mia/package.nix { };
 }
